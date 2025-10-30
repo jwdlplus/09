4e27968c1b53bcc2216e83ca82dcd1f6b2c09122
jwdlplus <jwqueen04@gmail.com>
2025-10-30 15:46:19 +0900

    09 실습2(2)

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 3
 
 int main(int argc, char *argv[])
 {
     int i;
     int grade[SIZE];
+    int sum=0;
+    
     printf("%i명 점수를 입력하세요.\n", SIZE);
     
     for(i=0;i<SIZE;i++)
-       scanf("%d", &grade[i]);
-    
+    {
+                       scanf("%d", &grade[i]);
+                       sum += grade[i];
+    }
+       
+    #if 0
     for(i=0;i<SIZE;i++)
        printf("grade[%i] = %i\n", i, grade[i]); 
-  
+    #endif
+    
+    printf("성적 평균 : %i\n", sum/SIZE); 
+    
     system("PAUSE");	
     return 0;
 }
